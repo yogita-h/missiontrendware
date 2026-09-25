1724baaafb28c2b5f4218c526cc1d9a975a74f5d
yogita-h <141008583+yogita-h@users.noreply.github.com>
2026-09-25 13:09:36 +0530

    changes

--- backend/Server.js
+++ backend/Server.js
@@ -1,17 +1,18 @@
-const  express=require("express")
-const connectDB=require("./config/db")
-const cors=require("cors")
-const app=express()
-const path = require("path")
-app.use(cors())
-app.use(express.json())
-const router=require("./routes/userrouter")
-connectDB()
-const PORT = process.env.PORT || 5000
-app.listen(PORT,()=>{
-    console.log(`Server is run on ${PORT} port`)
-})
-app.use("/api",router)
-app.use("/uploads", express.static(path.join(__dirname, "uploads")))
+const express = require("express");
+const connectDB = require("./config/db");
+const cors = require("cors");
+const path = require("path");
 
+const app = express();
 
+app.use(cors());
+app.use(express.json());
+
+connectDB();
+
+const router = require("./routes/userrouter");
+
+app.use("/api", router);
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+
+module.exports = app;
